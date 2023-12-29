77bb797cae8fe76c52f2f018cb2b96e6033f89f1
Rohan Sobha <r.r.sobha@student.tudelft.nl>
2023-12-29 21:04:08 +0000

    Add tests

--- tests/chat_model_test.py
+++ tests/chat_model_test.py
@@ -1,29 +1,107 @@
-from unittest.mock import MagicMock
 import pytest
+from langchain.chat_models.openai import ChatOpenAI
 from chatdoc.chat_model import ChatModel
 
-def test_set_vendor_name(monkeypatch):
-    monkeypatch.setattr('chatdoc.chat_model.Utils.get_env_variable', lambda _: 'openai')
-    chat_model = ChatModel()
-    assert chat_model.vendor_name == 'openai'
 
-def test_set_chat_model_name(monkeypatch):
-    monkeypatch.setattr('chatdoc.chat_model.Utils.get_env_variable', lambda _: 'gpt-3')
-    chat_model = ChatModel()
-    assert chat_model.chat_model_name == 'gpt-3'
+@pytest.fixture()
+def openai_chat_model(monkeypatch):
+    """
+    Function to test the chat model with OpenAI vendor.
 
-def test_load_chat_model_openai(monkeypatch):
-    monkeypatch.setattr('chatdoc.chat_model.Utils.get_env_variable', lambda _: 'openai')
-    monkeypatch.setattr('chatdoc.chat_model.ChatOpenAI', MagicMock())
-    chat_model = ChatModel()
-    assert isinstance(chat_model.chat_model, MagicMock)
+    Args:
+        monkeypatch: The monkeypatch fixture.
 
-def test_load_chat_model_huggingface(monkeypatch):
-    monkeypatch.setattr('chatdoc.chat_model.Utils.get_env_variable', lambda _: 'huggingface')
+    Returns:
+        ChatModel: An instance of the ChatModel class.
+
+    """
+    monkeypatch.setenv('CHAT_MODEL_VENDOR_NAME', 'openai')
+    monkeypatch.setenv('CHAT_MODEL_NAME', 'gpt-3')
+    monkeypatch.setenv('OPENAI_API_KEY', 'test')
+    yield ChatModel
+
+@pytest.fixture()
+def huggingface_chat_model(monkeypatch):
+    """
+    A test function that sets the environment variable 'CHAT_MODEL_VENDOR_NAME' to 'huggingface'
+    and yields the ChatModel object.
+
+    Args:
+        monkeypatch: A pytest fixture that allows modifying environment variables during testing.
+
+    Yields:
+        ChatModel: An instance of the ChatModel class.
+
+    """
+    monkeypatch.setenv('CHAT_MODEL_VENDOR_NAME', 'huggingface')
+    monkeypatch.setenv('CHAT_MODEL_NAME', 'BloombergGPT')
+    yield ChatModel
+
+@pytest.fixture()
+def invalid_vendor_chat_model(monkeypatch):
+    """
+    Test case for ChatModel with an invalid vendor name.
+    
+    Args:
+        monkeypatch: A pytest fixture used to modify environment variables.
+    
+    Yields:
+        ChatModel: An instance of the ChatModel class.
+    """
+    monkeypatch.setenv('CHAT_MODEL_VENDOR_NAME', 'mistral')
+    monkeypatch.setenv('CHAT_MODEL_NAME', 'mistral-7B')
+    yield ChatModel
+
+
+def test_set_vendor_name(openai_chat_model): # pylint: disable=W0621
+    """
+    Test case to verify the `vendor_name` property of the `chat_model_openai` object.
+
+    Args:
+        monkeypatch: Monkeypatch fixture for patching objects during testing.
+        chat_model_openai: Instance of the `ChatModel` class with the 'openai' vendor name.
+
+    Returns:
+        None
+    """
+    assert openai_chat_model().vendor_name == 'openai'
+
+def test_set_chat_model_name(openai_chat_model): # pylint: disable=W0621
+    """
+    Test case to verify if the chat model name is set correctly.
+    
+    Args:
+        chat_model_openai: An instance of the ChatModelOpenAI class.
+    
+    Returns:
+        None
+    """
+    assert openai_chat_model().chat_model_name == 'gpt-3'
+
+def test_load_chat_model_openai(openai_chat_model): # pylint: disable=W0621
+    """
+    Test case for loading the chat model from OpenAI.
+
+    Args:
+        chat_model_openai (MagicMock): The chat model object.
+
+    Returns:
+        None
+    """
+    assert isinstance(openai_chat_model().chat_model, ChatOpenAI)
+
+def test_load_chat_model_huggingface(huggingface_chat_model): # pylint: disable=W0621
+    """
+    Test case for the _load_chat_model method of the chat_model_huggingface object.
+    It checks if a NotImplementedError is raised when calling the _load_chat_model method.
+    """
     with pytest.raises(NotImplementedError):
-        ChatModel()
+        huggingface_chat_model()
+        
 
-def test_load_chat_model_invalid_vendor(monkeypatch):
-    monkeypatch.setattr('chatdoc.chat_model.Utils.get_env_variable', lambda _: 'invalid_vendor')
+def test_load_chat_model_invalid_vendor(invalid_vendor_chat_model): # pylint: disable=W0621
+    """
+    Test case to ensure that loading a chat model with an invalid vendor raises a ValueError.
+    """
     with pytest.raises(ValueError):
-        ChatModel()
+        invalid_vendor_chat_model()
